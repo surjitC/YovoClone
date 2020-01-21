ae3cca11d37c950c5d256406b15ee9ac9edec71a
Surjit Chowdhary <surjit.chowdhary@jci.com>
2020-01-21 19:55:03 +0530

    first commit

---
+++ Yovo Clone/Cell/ProfileCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Yovo Clone
+//
+//  Created by Surjit's iMac on 21/01/20.
+//  Copyright © 2020 Surjit. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Yovo Clone/ProfileViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewModel.swift
+//  Yovo Clone
+//
+//  Created by Surjit's iMac on 21/01/20.
+//  Copyright © 2020 Surjit. All rights reserved.
+//
+
+import Foundation
